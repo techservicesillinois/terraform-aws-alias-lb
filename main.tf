7eddf0d1599a5cd703fb2f969f4c5f7f9f8c87d2
Jon R. Roma <JonRoma@users.noreply.github.com>
2019-11-05 15:39:38 -0600

    Migrate to Terraform 0.12 (#1)

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 
 resource "aws_route53_record" "default" {
   # Zone and name of Route53 record being managed.
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "${var.hostname}"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = var.hostname
   type    = "A"
 
   alias {
     # Target of Route53 alias.
-    name                   = "${data.aws_lb.selected.dns_name}"
-    zone_id                = "${data.aws_lb.selected.zone_id}"
-    evaluate_target_health = "true"
+    name                   = data.aws_lb.selected.dns_name
+    zone_id                = data.aws_lb.selected.zone_id
+    evaluate_target_health = true
   }
 }
